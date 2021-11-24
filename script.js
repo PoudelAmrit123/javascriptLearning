d5393cedfa470765666598ecb64d8ef606973be7
Amrit Poudel <amritpoudel433@gmail.com>
2021-11-24 11:51:34 +0545

    Values and varilable Done

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let js= "Hello";
 if (js=== "Hello") alert (" Javascript is fun!");
+console.log ( 60+15-56*4);
+ console.log (" Jonas");
+ console.log ( "Amrit");
+ let Name = "Amu1";
+ console.log( "js");
+ 
+ console.log (Name);        
+ console.log (Name);
+ console.log (Name);
+ console.log (Name);
+
+ let years = "3";
+ console.log (years);
+ console.log ("years");
+
+ let jonashello= "27";
+ console.log ( jonashello);

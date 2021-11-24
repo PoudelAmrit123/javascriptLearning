a63472077573188067be8ef45c8b5dd87561805d
Amrit Poudel <amritpoudel433@gmail.com>
2021-11-24 15:04:31 +0545

    Operator and it precidance

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ console.log ( 60+15-56*4);
 
     // data type 
 
+   /*
     true;
     console.log (true);
 
@@ -48,3 +49,63 @@ console.log ( 60+15-56*4);
         console.log ("un");
 
         console.log ( typeof null);
+
+        */
+
+      
+      
+      /*
+        // constant variable 
+      
+        const birthyear = "1991";
+         console.log (birthyear);
+
+         birthyear = 1992;
+
+         */
+
+
+        //  operaters In javascript
+/*
+     let now = "1900";
+      let  agejonas = now - 1800 ;
+
+       console.log ( agejonas);
+       console.log ( agejonas *20 -1200 , 2 **3 );
+  
+       const firstname = "Amrit";
+       const lastname = "Poudel";
+
+        console.log ( firstname + ' ' + lastname);
+
+        let x= 10+5;
+         x += 10; // x= x+10
+          x *= 4 ; // x= x*4 
+        console.log(x);
+
+        console.log (now < agejonas );
+
+
+         let  z, y;
+          z=y= 25-10-5 ;
+          console.log (z , y , z+y);
+
+
+           now =100;
+           agejonas=100;
+          const average = (now + agejonas)/2;
+
+          console.log ( now , agejonas, average );
+
+          */
+
+          
+
+
+
+
+
+
+
+
+

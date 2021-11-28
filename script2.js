f517bd6fcfeafd2d683113f09b217ff127799417
Amrit Poudel <86000073+PoudelAmrit123@users.noreply.github.com>
2021-11-28 20:41:29 +0545

    Strict Mode

--- script2.js
+++ script2.js
@@ -1,1 +1,10 @@
 
+// using strict mode help us to find the error or  the bug easily
+
+let Hasdrive = false ;
+
+const Test = true ;
+
+if (Test)  Hasdrive= true ;
+
+if (Hasdrive ) console.log (' You are eligiable');

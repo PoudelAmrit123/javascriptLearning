dc77e609805d608d9ecc4cc6b31641c4a51f9a8e
Amrit Poudel <86000073+PoudelAmrit123@users.noreply.github.com>
2021-12-02 20:06:55 +0545

    Function Declration and expression

--- script2.js
+++ script2.js
@@ -11,6 +11,21 @@ if (Hasdrive ) console.log (' You are eligiable');
 
 
 
+ // function declartion
+
+  function calage (birthyear){
+      const age = 2021 - birthyear ;
+      return age ;
+    }
+
+    const result = calage( 1981);
+
+        console.log ( ` OK so the birthyear is ${result} `);
+
+  
+        
+       
+
 
 
 
